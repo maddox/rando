3e56a81996456b8b113a97f70a05cb339118a32f
Jon Maddox <maddox@github.com>
2018-06-22 14:46:15 -0400

    new log

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const MUPPETS = ["Kermit", "Miss Piggy", "Fozzie", "Animal", "Scooter", "Skeeter
 exports.randomMuppet = (req, res) => {
   const muppet = MUPPETS[Math.floor(Math.random()*MUPPETS.length)]
   console.log(`Returned a muppet: ${muppet}`)
-  console.log(`Are you sure you saw the muppet: ${muppet}`)
+  console.log(`Are you sure you saw the muppet?`)
   res.send(muppet)
 }
