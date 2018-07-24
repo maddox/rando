c3f8412b7fefa3fe3d6e78f10f264f953fbfdeb9
Jon Maddox <maddox@github.com>
2018-07-24 12:58:29 -0400

    broke it

--- index.js
+++
@@ -1,7 +0,0 @@
-const MUPPETS = ["Kermit", "Miss Piggy", "Fozzie", "Animal", "Scooter", "Skeeter", "Rowlf", "Gonzo", "Bunsen", "Beaker"]
-
-exports.randomMuppet = (req, res) => {
-  const muppet = MUPPETS[Math.floor(Math.random()*MUPPETS.length)]
-  console.log(`Returned another muppet! ${muppet}`)
-  res.send(`${muppet}`)
-}
